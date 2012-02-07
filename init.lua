f279dee5fa09e5c12c5d949f0c4d5c6aa8851d01
Piotr Matuszewski <multi@matusz.waw.pl>
2012-02-07 14:00:06 +0100

    init.lua

---
+++ init.lua
@@ -0,0 +1,205 @@
+-- chest lockable with an item
+
+local open_spec="invsize[8,9;]"
+			.."list[current_name;key;0,0;1,1;]"
+			.."list[current_name;lock;0,2;1,1;]"
+			.."list[current_name;main;2,0;5,4;]"
+			.."list[current_player;main;0,5;8,4;]"
+
+local locked_spec="invsize[8,9;]"
+			.."list[current_name;key;0,0;1,1;]"
+			.."list[current_player;main;0,5;8,4;]"
+
+minetest.register_craft({
+    output = 'klchest:lock',
+    recipe = {
+        {'','default:steel_ingot',''},
+        {'default:steel_ingot','','default:steel_ingot'},
+        {'','default:steel_ingot',''},
+    }
+})
+
+minetest.register_craftitem('klchest:lock', {
+    description = 'Lock',
+    inventory_image = 'chest_lock.png',
+})
+
+minetest.register_tool('klchest:key', {
+    description = 'Key',
+    stack_max = 1,
+    inventory_image = 'chest_key.png',
+    tool_digging_properties = {
+        basetime = 0,
+        dt_weight = 0,
+        dt_crackiness = 0,
+        dt_crumbliness = 0,
+        dt_cuttability = 0,
+        basedurability = 0,
+        dd_weight = 0,
+        dd_crackiness = 0,
+        dd_crumbliness = 0,
+        dd_cuttability = 0,
+    },
+    on_use = function(itemstack, user, pointed_thing)
+--        print("klchest:key.on_use()")
+        local meta=minetest.env:get_meta(pointed_thing.under)
+        --print("MN:".. meta.name)
+        --if meta.name~="klchest:item_lockable_chest" then
+        --    return
+        --end
+        local inven = meta:get_inventory()
+        if inven==nil then
+            return
+        end
+        local lock_s = inven:get_stack("lock",1)
+        print("I:"..dump(itemstack:to_string()))
+        print("LOCK:"..dump(lock_s:to_string())..";"..lock_s:get_wear())
+        if itemstack:to_string()==lock_s:to_string() then
+
+            print("EQUAL")
+          local status = meta:get_string("status")
+          if status=="locked" and lock==key then
+              meta:set_inventory_draw_spec(open_spec)
+              meta:set_string("status", "unlocked")
+          elseif status=="unlocked" and lock ~= "" then
+              meta:set_inventory_draw_spec(locked_spec)
+              meta:set_string("status", "locked")
+          end
+          status = meta:get_string("status")
+		  meta:set_infotext("Chest is "..status)
+        end
+    end,
+})
+
+minetest.register_craft({
+    output = 'klchest:key',
+    recipe = {
+        {'default:steel_ingot','default:steel_ingot','default:steel_ingot'},
+        {'','','default:steel_ingot'},
+    }
+})
+
+minetest.register_craft({
+	output = 'klchest:item_lockable_chest',
+	recipe = {
+		{'default:chest', 'klchest:lock'},
+	}
+})
+
+minetest.register_craft({
+	output = 'klchest:item_lockable_chest',
+	recipe = {
+		{'default:wood', 'default:wood', 'default:wood'},
+		{'default:wood', 'klchest:lock', 'default:wood'},
+		{'default:wood', 'default:wood', 'default:wood'},
+	}
+})
+
+minetest.register_node("klchest:item_lockable_chest", {
+	description = "Chest-bricky",
+--    diggable = false,
+	tile_images = {"default_chest_top.png", "default_chest_side.png",
+		"default_chest_side.png", "default_chest_side.png",
+		"default_chest_side.png", "chest_chest_front.png"},
+	inventory_image = minetest.inventorycube("default_chest_top.png", 
+        "chest_chest_front.png", "default_chest_side.png"),
+	paramtype = "facedir_simple",
+	metadata_name = "generic",
+	material = minetest.digprop_woodlike(3.0),
+})
+local empty_twenty={
+            "", "", "", "", "",
+            "", "", "", "", "",
+            "", "", "", "", "",
+            "", "", "", "", "",
+        }
+minetest.register_on_placenode(function(pos, newnode, placer)
+	if newnode.name == "klchest:item_lockable_chest" then
+		local meta = minetest.env:get_meta(pos)
+        local inven = meta:get_inventory()
+		inven:set_list("key", {""})
+		inven:set_list("lock", {""})
+		inven:set_list("main", empty_twenty)
+		meta:set_inventory_draw_spec(open_spec)
+        meta:set_string("status", "unlocked")
+		meta:set_infotext("Chest is unlocked")
+	end
+end)
+
+minetest.register_on_punchnode(
+  function(pos, node)
+      if node.name=="klchest:item_lockable_chest" then
+          --minetest.debug("bricky chest punched")
+          local meta = minetest.env:get_meta(pos)
+          local inven = meta:get_inventory()
+          local status = meta:get_string("status")
+          local lock = inven:get_list("lock")[1]
+          local key = inven:get_list("key")[1]
+          local lock_s = inven:get_stack("lock",1)
+          local key_s = inven:get_stack("key",1)
+          print("KEY:"..dump(key_s:to_string())..";"..key_s:get_wear())
+          print("LOCK:"..dump(lock_s:to_string())..";"..lock_s:get_wear())
+
+          if lock=="klchest:key" and key=="klchest:key" then
+              local w = math.random(65536)
+              print("W:"..w)
+              key_s:add_wear(w)
+              inven:set_stack("key",1,key_s)
+              lock_s:add_wear(w)
+              inven:set_stack("lock",1,lock_s)
+          end
+          print("KEY:"..dump(key_s:to_string())..";"..key_s:get_wear())
+          print("LOCK:"..dump(lock_s:to_string())..";"..lock_s:get_wear())
+          
+          --[[
+          print("STATUS:"..status)
+          print("INV-KEY:"..dump(inven:get_list("key")))
+          print("INV-LOCK:"..dump(inven:get_list("lock")))
+          if inven:get_list("lock") == nil or lock=="" then
+              print("NO LOCK")
+          end
+          --]]
+          --print("LOCK-SIZE:"..inven:get_size("lock"))
+          --print("MAIN-SIZE:"..inven:get_size("main"))
+          --print("MAIN:"..dump(inven:get_list("main")))
+          --print("MAIN:"..dump(empty_twenty))
+
+          local empty = true
+          if lock~="" then
+              empty = false
+          elseif key ~="" then
+              empty = false
+          else
+              for i,v in pairs(inven:get_list("main")) do
+                  if v~="" then
+                      empty = false
+                      break
+                  end
+              end
+          end
+--          if lock=="klchest:key" then
+--              print("KEY:"..dump(key)..";"..key:get_wear())
+--              print("LOCK:"..dump(lock)..";"..lock:get_wear())
+          --    print("KM:"..key:set_string("key_id","ala"))
+--          end
+
+          --print("MAIN empty?:"..(empty and 'empty' or 'not-empty'))
+          --print("RND:"..math.random(3))
+          if empty then
+              meta:set_allow_removal(true)
+          else
+              meta:set_allow_removal(false)
+          end
+          if status=="locked" and lock==key then
+              meta:set_inventory_draw_spec(open_spec)
+              meta:set_string("status", "unlocked")
+          elseif status=="unlocked" and lock ~= "" then
+              meta:set_inventory_draw_spec(locked_spec)
+              meta:set_string("status", "locked")
+          end
+          status = meta:get_string("status")
+		  meta:set_infotext("Chest is "..status)
+          --print("CHANGE:"..status)
+      end
+  end
+)
